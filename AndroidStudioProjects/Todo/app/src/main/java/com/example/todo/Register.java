247fce91bc9e3bad043a76b32b97386179123a42
Muhibb-Shaikh-7 <muhibbshaikh76@gmail.com>
2024-02-19 17:23:19 +0530

    Register xml added and minor changes in Login xml

--- AndroidStudioProjects/Todo/app/src/main/java/com/example/todo/Register.java
+++ AndroidStudioProjects/Todo/app/src/main/java/com/example/todo/Register.java
@@ -2,13 +2,31 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
+    TextView lgn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        lgn=findViewById(R.id.clkreg);
+
+        lgn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
