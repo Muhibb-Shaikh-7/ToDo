79f34e33088544eafc2c32591e85eab30c46b916
Muhibb-Shaikh-7 <muhibbshaikh76@gmail.com>
2024-02-19 16:25:08 +0530

    Login xml added

--- AndroidStudioProjects/Todo/app/src/main/java/com/example/todo/Login.java
+++ AndroidStudioProjects/Todo/app/src/main/java/com/example/todo/Login.java
@@ -2,13 +2,31 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    TextView reg;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        reg=findViewById(R.id.clkreg);
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Register.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
